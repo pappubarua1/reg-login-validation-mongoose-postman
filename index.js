fcda1b84f4233edab5036f2c63bfdff7b2fb8e5a
pappubarua1 <48848746+pappubarua1@users.noreply.github.com>
2021-07-21 04:06:38 +0600

    File Name
    models-Pin.js,User.js
    routes- pins.js , users.js
    main file index.js

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const userRoute = require('./routes/users');
+const pinRoute = require('./routes/pins');
+
+const app = express();
+
+dotenv.config();
+
+app.use(express.json())
+mongoose
+.connect( process.env.MONGO_URL, 
+    {useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+})
+.then(()=>{
+console.log("MongoDB Connected!");
+})
+.catch(err=>console.log(err));
+
+app.use('/api/users',userRoute);
+app.use('/api/pins',pinRoute);
+
+app.listen(8000,()=>{
+    console.log('backend is running!!');
+});
